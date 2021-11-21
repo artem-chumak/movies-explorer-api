abe1a6370d70e70021b25a082575498eb20ec5db
Artem Chumak <artem.4x@gmail.com>
2021-11-21 17:25:38 +0300

    feat(middlewares): add middlewares

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { errors, celebrate, Joi } = require('celebrate');
+const cookieParser = require('cookie-parser');
+const usersRouter = require('./routes/users');
+const { creatUser, login, logout } = require('./controllers/users');
+const moviesRouter = require('./routes/movies');
+const auth = require('./middlewares/auth');
+const errorsControll = require('./controllers/errors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const NotFoundError = require('./errors/NotFoundError');
 
 const { PORT = 3000 } = process.env;
 
---
+++ middlewares/auth.js
@@ -0,0 +1,14 @@
+const { NODE_ENV, JWT_SECRET } = process.env;
+const jwt = require('jsonwebtoken');
+const UnauthorizedUserError = require('../errors/UnauthorizedUserError');
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+    const payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    req.user = payload;
+    return next();
+  } catch (error) {
+    throw new UnauthorizedUserError('Нет авторизации');
+  }
+};
